90938ad025b96e9050e38551eb011e04d75764de
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-06-23 23:20:52 +0530

    Time: 0 ms (100%), Space: 19.5 MB (5.38%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        
+        def solve(input, output, arr):
+
+            if len(input) == 0:
+                arr.append(output)
+                return arr
+
+            op1 = output.copy()
+            op2 = output.copy()
+            
+            op2.append(input[0])
+
+            input = input[1:]
+
+            solve(input, op1, arr)
+            solve(input, op2, arr)
+            return arr
+
+        result = []
+        solve(nums, [], result)
+        return result
