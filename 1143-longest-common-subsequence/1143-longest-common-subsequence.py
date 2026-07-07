05953c2b3f0152a217167f26f27955504f5f2ded
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-07-07 22:27:42 +0530

    Time: 699 ms (20.91%), Space: 27.6 MB (85.96%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -4,26 +4,26 @@ class Solution:
 
         n = len(text1)
         m = len(text2)
-        self.t = [[-1] * (m+1) for _ in range(n+1)]
+        t = [[-1] * (m+1) for _ in range(n+1)]
 
-        return self.LCS(text1, text2, n , m)
+        return self.LCS(text1, text2, n , m, t)
 
 
-    def LCS(self, X, Y , n , m):
+    def LCS(self, X, Y , n , m, t):
 
         # base case
         if n == 0 or m == 0:
             return 0
 
-        if self.t[n][m] != -1:
-            return self.t[n][m]
+        if t[n][m] != -1:
+            return t[n][m]
 
         elif X[n-1] == Y[m-1]:
-            self.t[n][m] =  1 + self.LCS(X, Y, n-1, m-1)
-            return self.t[n][m]
+            t[n][m] =  1 + self.LCS(X, Y, n-1, m-1, t)
+            return t[n][m]
 
         elif X[n-1] != Y[m-1]:
-            c1 = self.LCS(X, Y , n-1, m)
-            c2 = self.LCS(X, Y, n, m-1)
-            self.t[n][m] = max(c1, c2)
-            return self.t[n][m]
+            c1 = self.LCS(X, Y , n-1, m, t)
+            c2 = self.LCS(X, Y, n, m-1, t)
+            t[n][m] = max(c1, c2)
+            return t[n][m]
