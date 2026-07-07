aa3f5009eab3c0930b24bb7670e23ddf2d25bf96
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-07-07 23:01:16 +0530

    Time: 466 ms (31.22%), Space: 44.3 MB (58.2%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -4,13 +4,9 @@ class Solution:
 
         n = len(text1)
         m = len(text2)
-        t = [[-1] * (m+1) for _ in range(n+1)]
+        t = [[0] * (m+1) for _ in range(n+1)]
 
-        for i in range(n+1):
-            t[i][0] = 0
 
-        for j in range(m+1):
-            t[0][j] = 0
 
         for i in range(1, n+1):
             for j in range(1, m+1):
