2998c354a42e74900b6e728f7c357d9103c8774c
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-07-13 22:09:42 +0530

    Time: 551 ms (19.66%), Space: 21 MB (67.82%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minInsertions(self, s: str) -> int:
+
+        r = s[::-1]
+
+        n = len(s)
+        m = n
+
+        t = [[0] * (m+1) for _ in range(n+1)]
+
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+
+                if s[i-1] == r[j-1]:
+                    t[i][j] = 1 + t[i-1][j-1]
+
+                elif s[i-1] != r[j-1]:
+                    c1 = t[i-1][j]
+                    c2 = t[i][j-1]
+
+                    t[i][j] = max(c1, c2)
+
+        # its actually lps 
+        lps = t[n][m]
+
+        return n - lps
+
+
