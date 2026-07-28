378d9e58801cec003f9292158c018b50e1529ab7
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-07-28 23:29:28 +0530

    Time: 0 ms (100%), Space: 19.3 MB (60.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -7,23 +7,35 @@ class Solution:
 
         n = len(nums)
         t = [-1] * (n+1)
+
+        t[0] = 0
+        t[1] = nums[0]
         
-        def solve(n):
+        # def solve(n):
 
-            if n <= 0:
-                return 0
+        #     if n <= 0:
+        #         return 0
 
-            if n == 1:
-                return nums[0]
+        #     if n == 1:
+        #         return nums[0]
 
-            if t[n] != -1:
-                return t[n]
+        #     if t[n] != -1:
+        #         return t[n]
 
-            choice1 = nums[n-1] + solve(n-2) 
-            choice2 = solve(n-1)
+        #     choice1 = nums[n-1] + solve(n-2) 
+        #     choice2 = solve(n-1)
 
-            t[n] = max(choice1 , choice2)
-            return t[n]
+        #     t[n] = max(choice1 , choice2)
+        #     return t[n]
 
 
-        return solve(n)
+        # return solve(n)
+
+        for i in range(2, n+1):
+
+            c1 = nums[i-1] + t[i-2]
+            c2 = t[i-1]
+
+            t[i] = max(c1, c2)
+
+        return t[n]
