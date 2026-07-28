748a98cf0aff09fe34cfca44970bd54568684979
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-07-28 23:27:25 +0530

    Time: 0 ms (100%), Space: 19.4 MB (5.97%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,8 +3,11 @@ from functools import cache
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
+
+
+        n = len(nums)
+        t = [-1] * (n+1)
         
-        @cache
         def solve(n):
 
             if n <= 0:
@@ -13,10 +16,14 @@ class Solution:
             if n == 1:
                 return nums[0]
 
+            if t[n] != -1:
+                return t[n]
+
             choice1 = nums[n-1] + solve(n-2) 
             choice2 = solve(n-1)
 
-            return max(choice1 , choice2)
+            t[n] = max(choice1 , choice2)
+            return t[n]
 
-        n = len(nums)
+
         return solve(n)
