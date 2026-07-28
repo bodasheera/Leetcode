692bce445803bce391f3e8e7195b8800224019bf
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-07-28 23:08:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.93%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -8,17 +8,30 @@ class Solution:
 
         t = [-1] * (n+1) 
 
-        def solve(n):
+        # def solve(n):
             
-            # base case 
-            if n <= 2:
-                return n
+        #     # base case 
+        #     if n <= 2:
+        #         return n
 
-            if t[n] != -1:
-                return t[n]
+        #     if t[n] != -1:
+        #         return t[n]
 
-            # hypothesis + induction
-            t[n] =  solve(n-2) + solve(n-1)
-            return t[n]
+        #     # hypothesis + induction
+        #     t[n] =  solve(n-2) + solve(n-1)
+        #     return t[n]
 
-        return solve(n)
+        # return solve(n)
+
+        if n <= 2:
+            return n
+        
+        t[0] = 0
+        t[1] = 1
+        t[2] = 2
+
+        for i in range(3, n+1):
+
+            t[i] = t[i-2] + t[i-1]
+
+        return t[n]
