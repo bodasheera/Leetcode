681323700b4b5c127337afe1df0ce3554f72b0f8
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-07-29 07:11:29 +0530

    Time: 2 ms (67.14%), Space: 19.4 MB (25.13%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -7,32 +7,45 @@ class Solution:
 
         t = [-1] * (n+1)
 
-        def solve(n):
+        # def solve(n):
 
-            # base case
-            # when we start no cost 
-            # we can start at 0 and 1
+        #     # base case
+        #     # when we start no cost 
+        #     # we can start at 0 and 1
 
-            if n <= 1:
-                return 0
+        #     if n <= 1:
+        #         return 0
 
-            if t[n] != -1:
-                return t[n]
+        #     if t[n] != -1:
+        #         return t[n]
 
-            # hypothesis
-            # we can end at n or n-1
-            # pay the cost and solve for smaller input 
+        #     # hypothesis
+        #     # we can end at n or n-1
+        #     # pay the cost and solve for smaller input 
 
-            # use n and solve smaller input
-            c1 = cost[n-1] + solve(n-1) 
+        #     # use n and solve smaller input
+        #     c1 = cost[n-1] + solve(n-1) 
 
-            # use n-1 and solve smaller input
-            c2 = cost[n-2] + solve(n-2)
+        #     # use n-1 and solve smaller input
+        #     c2 = cost[n-2] + solve(n-2)
 
-            # induction
-            t[n] = min(c1, c2)
+        #     # induction
+        #     t[n] = min(c1, c2)
 
-            return t[n]
+        #     return t[n]
  
 
-        return solve(n)
+        # return solve(n)
+
+        # base case
+        t[0] = 0
+        t[1] = 0
+
+        for i in range(2, n+1):
+
+            c1 = cost[i-1] + t[i-1]
+            c2 = cost[i-2] + t[i-2]
+
+            t[i] = min(c1, c2)
+
+        return t[n]
