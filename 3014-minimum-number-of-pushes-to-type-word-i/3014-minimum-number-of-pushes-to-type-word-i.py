476fd6630b885c5f7c2a7dbe0e9bceffdbbd39bc
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-07-30 11:05:30 +0530

    Time: 0 ms (100%), Space: 19.2 MB (83.33%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        
+
+        keys = [0] * 8
+
+        pos = 0 
+
+        for w in word:
+
+            if keys[pos] < 3:
+                keys[pos] += 1
+                pos += 1
+                pos = pos % 8
+
+            else:
+                pos += 1
+                keys[pos] += 1
+                pos += 1
+                pos = pos % 8
+
+        total = 0
+
+        for k in keys:
+
+            total += k * (k+1) // 2
+
+        return total
