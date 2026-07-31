ef5c0c8210a9afa81cc9533249673e242a2db8af
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-07-31 21:25:17 +0530

    Time: 96 ms (76.59%), Space: 20 MB (48.16%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -1,43 +1,47 @@
+from collections import Counter
+
 class Solution:
     def minimumPushes(self, word: str) -> int:
 
-        word_map = {}
-        max_ct = -1
+        # word_map = {}
+        # max_ct = -1
 
-        for c in word:
-            word_map[c] = word_map.get(c, 0) + 1
-            if max_ct < word_map[c]:
-                max_ct = word_map[c]
+        # for c in word:
+        #     word_map[c] = word_map.get(c, 0) + 1
+        #     if max_ct < word_map[c]:
+        #         max_ct = word_map[c]
 
-        counter = [[] for _ in  range(max_ct+1)]
+        # counter = [[] for _ in  range(max_ct+1)]
 
-        for c, ct in word_map.items():
-            counter[ct].append(c)
+        # for c, ct in word_map.items():
+        #     counter[ct].append(c)
 
-        multiplier = 1
-        pos = 0
+        # multiplier = 1
+        # pos = 0
+        # cost = 0
+
+
+        # for i in range(max_ct, 0, -1):
+
+        #     if len(counter[i]) == 0:
+        #         continue 
+
+        #     else:
+        #         for c in counter[i]:
+        #             multiplier = pos // 8 + 1
+        #             cost = cost + multiplier * i
+        #             pos = pos + 1
+
+        # return cost 
+
+        counts = Counter(word)
         cost = 0
+        freqs = sorted(counts.values(), reverse=True)
 
+        for i in range(len(freqs)):
 
-        for i in range(max_ct, 0, -1):
+            multiplier = i // 8 + 1
 
-            if len(counter[i]) == 0:
-                continue 
+            cost = cost + multiplier * freqs[i]
 
-            else:
-                for c in counter[i]:
-                    multiplier = pos // 8 + 1
-                    cost = cost + multiplier * i
-                    pos = pos + 1
-
-        return cost 
-
-
-
-
-
-
-        
-        
-
-        
+        return cost
