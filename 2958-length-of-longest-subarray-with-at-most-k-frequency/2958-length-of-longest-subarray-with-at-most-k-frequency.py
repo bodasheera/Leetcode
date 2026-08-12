d44093677ba23d8f8054107968e32e9645ed7a1a
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-08-12 11:03:20 +0530

    Time: 279 ms (27.94%), Space: 35.3 MB (72.85%) - LeetHub

---
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        
+
+        # variable size sliding window
+        i = 0
+        j = 0
+        counter = {}
+
+        res = 0
+
+        while j < len(nums):
+
+            # calculation
+            counter[nums[j]] = counter.get(nums[j], 0) + 1
+
+            # condition < k
+            # cond = max(counter.values()) commenting as TLE
+            cond = counter[nums[j]] # basically only current ele is screwing up our logic 
+
+            # condition < k . valid window
+            if cond <= k:
+                res = max(res, j - i + 1)
+                j += 1
+
+            # invalid condition
+            elif cond > k:
+
+                while cond > k:
+                    counter[nums[i]] = counter.get(nums[i], 0) - 1
+                    i += 1
+                    cond = counter[nums[j]] # basically only current ele is screwing up our logic 
+
+                j += 1
+                
+        return res
+                
