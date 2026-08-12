66d8dfa48c034d00277dd5dd0f3b4b557652d052
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-08-12 19:29:54 +0530

    Time: 0 ms (100%), Space: 19.3 MB (30.87%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -5,6 +5,9 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+
+
+
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         
         def solve(node1, node2):
