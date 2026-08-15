557a7570dbd85aee10e4c69bfa01a7978a84601b
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-08-15 10:13:28 +0530

    Time: 11 ms (24.4%), Space: 22.5 MB (71.39%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,44 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        if len(intervals) <= 1:
+            return intervals
+
+
+        intervals.sort()
+        res = [intervals[0]]
+        i =1
+        
+
+        while i < len(intervals):
+
+            s1, e1 = res[-1]
+            s2, e2 = intervals[i]
+
+            # overlapping intervals
+            if s2 <= e1 and s1 <= e2:
+
+                # merge intervals
+                s = min(s1,s2)
+                e = max(e1,e2)
+
+                res.pop()
+                res.append([s,e])
+                i += 1
+                
+
+            # not overlapping intervals
+            elif s1 > e2 or s2 > e1:
+                res.append(intervals[i])
+                i += 1
+
+        return res
+
+
+
+
+
+
+
+
+
