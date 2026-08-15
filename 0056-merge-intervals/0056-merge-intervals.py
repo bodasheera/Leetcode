0796f4a70a7cf38157c91fed5ebee25c3a4dd1b7
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-08-15 10:14:27 +0530

    Time: 7 ms (71.62%), Space: 22.4 MB (86.35%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -4,12 +4,12 @@ class Solution:
         if len(intervals) <= 1:
             return intervals
 
-
+        # first step sort it 
         intervals.sort()
+
         res = [intervals[0]]
         i =1
         
-
         while i < len(intervals):
 
             s1, e1 = res[-1]
@@ -22,13 +22,19 @@ class Solution:
                 s = min(s1,s2)
                 e = max(e1,e2)
 
+                # merging so remove top of the res
                 res.pop()
+
+                # add back the merged interval
                 res.append([s,e])
+
                 i += 1
                 
 
             # not overlapping intervals
             elif s1 > e2 or s2 > e1:
+
+                # add the new interval
                 res.append(intervals[i])
                 i += 1
 
