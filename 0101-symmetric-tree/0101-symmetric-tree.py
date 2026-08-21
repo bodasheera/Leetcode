2b0686bee7343bd50c57b35d442102a5083bee8c
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-08-21 15:39:03 +0530

    Time: 0 ms (100%), Space: 19.5 MB (18.41%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+
+        
+        def solve(tree1, tree2):
+
+            # base
+            if tree1 == None and tree2 == None:
+                return True
+
+            if tree1 == None and tree2 != None:
+                return False
+
+            
+            if tree2 == None and tree1 != None:
+                return False
+
+            # hypothesis and induction
+            return tree1.val == tree2.val and solve(tree1.left, tree2.right) and solve(tree1.right, tree2.left)
+
+        return solve(root.left, root.right)
+
+        
+
+
+
+            
+
+            
