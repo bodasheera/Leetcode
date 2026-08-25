06409045ad874136edf77d31778eb8c49faeb1c0
Vignesh Saikrishna Mallya <viggi.codes@gmail.com>
2026-08-25 16:24:04 +0530

    Time: 0 ms (100%), Space: 19.2 MB (57.81%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.py
+++ 3945-digit-frequency-score/3945-digit-frequency-score.py
@@ -3,16 +3,11 @@ from collections import Counter
 class Solution:
     def digitFrequencyScore(self, n: int) -> int:
         
-        freq = defaultdict(int)
+        total = 0
 
         while n:
             n, d = divmod(n , 10)
-            freq[d] += 1
-
-        total = 0
-        for d , count in freq.items():
-
-            total += d * count
+            total += d
 
         return total
 
